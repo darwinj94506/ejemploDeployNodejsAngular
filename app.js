0fb371828faf0eadd61b3288bb8cba8b167a5afd
darwinj94 <darwinj94@hotmail.com>
2019-06-18 19:46:22 -0500

    hasta aqui esta subido a heroku, todo esta correctamente

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/', indexRouter);
+// app.use('/', indexRouter); 
 app.use('/api', usersRouter);
 app.use('/',express.static('client/dist/client',{redirect:false}));
 
